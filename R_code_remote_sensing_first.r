85bbebbf104e1c497c6387212ed90b354a85cc04
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2021-04-07 19:14:31 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -6,5 +6,15 @@ library(raster)
 
 #uso la funzione brick  
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
+p224r63_2011
 
 plot(p224r63_2011)
+
+#cambio di colore della legenda
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+
+plot(p224r63_2011, col= cl)
+
+# cambio di colore -> new
+cl <- colorRampPalette(c("blue","green"," grey", "red","magenta","yellow")) (100)
+plot(p224r63_2011, col= cl)
