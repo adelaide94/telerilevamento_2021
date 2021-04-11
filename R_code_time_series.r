905cbfb3fb035faf46651fc023abaf65699bca1c
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2021-04-11 19:00:39 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,8 +1,13 @@
-# TIME SERIES ANALYSS
-# GREENLAND INCREASE OF TEMPERATURE
+# TIME SERIES ANALYS
+# GREENLAND INCREASE OF TEMPERATURE potenziale
 # data code from Emanuela Cosma
 
 #installo il pacchetto raster e lo richiamo in R
 library(raster)
 
 setwd("C:/lab/greenland")
+# per importare le immagini .tif uso la funzione raster dell'omonimo pacchetto
+lst_2000 <- raster("lst_2000.tif")
+plot(lst_2000)
+lst_2005 <- raster("lst_2000.tif")
+plot(lst_2005)
