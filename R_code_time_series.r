b4046b076a0b898181896e4a12ee5ab1a5024f5c
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2021-04-11 09:47:26 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,8 @@
+# TIME SERIES ANALYSS
+# GREENLAND INCREASE OF TEMPERATURE
+# data code from Emanuela Cosma
+
+#installo il pacchetto raster e lo richiamo in R
+library(raster)
+
+setwd("C:/lab/greenland")
