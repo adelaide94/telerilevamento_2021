f1eaff80bbdd969b83dfdabe76ec772b444099c0
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2021-04-11 09:23:20 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -84,7 +84,8 @@ plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")plotRGB(p224r63_1988, r=3, g=
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
 
-#Multitemporale set
+# DAY 5
+#Multitemporale setDEV
 p224r63_1988 <- brick("p224r63_1988_masked.grd")
 plot(p224r63_1988)
 
@@ -95,9 +96,17 @@ plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
 # esercizio di confronto tra amazzonia 2011 e 1988 con sistema RGD con banda dell'infrarosso
 par(mfrow=c(2,1))
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
-plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")???
+plotRGB(p224r63_2011, r=4,
+        g=3, b=2, stretch="Lin")???
 
-# es di confronto tra amazzonia 2011 e 1988 con sistema RGD con banda dell'infrarosso con gli strecth par ed hista
+# es di confronto MULTITEMP tra amazzonia 2011 e 1988 con sistema RGD con banda dell'infrarosso con gli strecth par ed hista
+library(raster)
+setwd("C:/lab/")
+
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+pdf("multitemp.pdf")
 par(mfrow=c(2,2))
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
