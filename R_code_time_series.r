8fc59f38bd171da6f0fde13713a1b5decf65fe31
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2021-04-13 00:08:41 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -37,3 +37,46 @@ plot(TGr)
 plotRGB(TGr, 1, 2, 3, stretch="Lin")
 plotRGB(TGr, 2, 3, 4, stretch="Lin")
 plotRGB(TGr, 4, 3, 2, stretch="Lin")
+
+
+#day 2
+#installo il pacchetto rasterVis
+install.packages("rasterVis")
+
+#richiamo il pachetto in r
+library(rasterVis)
+# usiamo levelplot per carcare tutto il blocco TGr ed avere una sola legenda
+levelplot(TGr)
+
+#esercizio associo i valori di TGr alla mappa lst_2000 
+levelplot(TGr$lst_2000)
+# cambio la palette dell'immagine
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+
+# voglio visualizzare
+levelplot(TGr,col.regions=cl,names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#argomento main mi mette il titolo al grafico
+levelplot(TGr,col.regions=cl,main="Lst varietion in time",names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+# Inizio dell'uso dei dati melt_greenlad provenienti dal sensore a microonde NIBUS7 per l'analisi temporali rispetto alla variazionedel giaccio in groellandia
+
+#esercizio crea una lista melt costituitita da tutti i file melt della cartella greenland
+meltlist <- list.files(pattern="melt")
+meltlist
+# applico lapply alla lista meltlist 
+melt_import<-lapply(meltlist,raster)
+melt_import
+
+# faccio l'unione di tutti i file che ho importato con la funzione stack
+melt <- stack (melt_import)
+melt
+#visualizzo
+levelplot(melt)
+
+# posso mostrare la differenza in termini di scimento del ghiacchio tra l'immagine 1978 e quella del 2007              
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt 
+ #colorRampPalette
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+levelplot(melt_amount, col.region=clb)
