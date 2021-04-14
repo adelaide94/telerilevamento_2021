5c1d0383c7b3100fa8e485849db7f4051910cd3c
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2021-04-14 13:03:29 +0200

    Create R_code_.copernicus.r

---
+++ R_code_.copernicus.r
@@ -0,0 +1,18 @@
+# vISUALIZZARE LE IMMAGINE DI COPERNICUS
+#CONTROLLO DI AVERE LE LIBRERIE
+library(raster)
+
+#liberia per leggere il formato .ncdf4 e devo istallare il pacchettp
+install.packages("ncdf4")
+library(ncdf4)
+
+# setto la cartella dei dati
+setwd("C:/lab/")
+ 
+ #devo associare il file scaricato al nome 
+vegetation_Properties <- raster("c_gls_FAPAR_202006130000_GLOBE_PROBAV_V1.5.1.nc")
+vegetation_Properties
+
+# non uso la RGB ho un singolo strado quindi decioo la scala di colori
+cl <- colorRampPalette(c('light blue','green','red','yellow'))(100) #
+plot(vegetation_Properties, col=cl)
