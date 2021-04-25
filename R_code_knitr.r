5756960148ad7a2cede9d0a4f533a8d25acea4de
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2021-04-25 18:45:43 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,9 @@
+#
+
+# Creo il set della working directory
+setwd("C:/lab/")
+
+# Uso il pacchetto knitr per includere in R un codice esterno presente nella cartella, creo un report e lo risalvo nella cartella
+#chiamo knitr
+
+library(knitr)
