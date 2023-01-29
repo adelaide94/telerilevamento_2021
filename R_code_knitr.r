d21b12fcb2f0fa4fa5871b31aa3927640fd4b927
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2023-01-29 08:54:09 +0100

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -5,5 +5,7 @@ setwd("C:/lab/")
 
 # Uso il pacchetto knitr per includere in R un codice esterno presente nella cartella, creo un report e lo risalvo nella cartella
 #chiamo knitr
-
 library(knitr)
+
+#uso stich per generare report,  le figure e permette di saalvare nella cartella originale
+stitch("R_code_time_series.r.rtf", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
