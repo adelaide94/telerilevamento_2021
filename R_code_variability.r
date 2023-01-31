9921e421aaca58402d036036ac0dd73c4be240e4
adelaide94 <80513567+adelaide94@users.noreply.github.com>
2023-01-31 22:19:14 +0100

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -46,3 +46,35 @@ plot(ndvisd5, col=clsd) #plotto
 sentpca <- rasterPCA(sent)
 plot(sentpca$map) # analisi multivariata dell'immagine
 summary(sentpca$model) #PC1 contains 67.3604% of the original information
+
+pc1 <- sentpca$map$PC1 #prendo la prima componente principale
+pc1sd5 <- focal(pc1, w=matrix(1/25,nrow=5,ncol=5), fun=sd) #calcolo sd sulla pc1 con moving window 5x5
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #creo scala di colori
+plot(pc1sd5, col=clsd) #plotto
+
+# Con la funzione source carico il codice "source_test_lezione.r" in modo che R lo esegua
+source("source_test_lezione.r") # "source_test_lezione.r" calcola la sd con finestra 7x7
+# carico il codice già creato "source_ggplot.r"
+source("source_ggplot.r")
+
+# uso ggplot per plottare le mappa di sd della pc1
+p1 <- ggplot()+ #creo finestra vuota
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill= layer))+ #definisco geometria raster della mappa 
+scale_fill_viridis()+ #uso scala di colore viridis default, funzione che permette di inserire scale di colore ben visibili anche in condizioni di problematiche ottiche
+ggtitle("Standard deviation of PC1 by viridis colour scale") #aggiungo titolo
+
+#uso legenda "magma"
+p2 <- ggplot()+ #creo finestra vuota
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill= layer))+ #definisco geometria
+scale_fill_viridis(option = "magma")+ #uso scala di colore viridis magma visualizza chiaramente livelli di sd alti
+ggtitle("Standard deviation of PC1 by magma colour scale") #aggiungo titolo
+
+#uso legenda "turbo"
+p3 <- ggplot()+ #creo finestra vuota
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill= layer))+ #definisco geometria
+scale_fill_viridis(option = "turbo")+
+ggtitle("Standard deviation of PC1 by turbo colour scale") #aggiungo titolo
+
+grid.arrange(p1, p2, p3, nrow=1) #metto insieme le tre mappe in una pagina 
+
+
